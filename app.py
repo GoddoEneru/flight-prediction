dfc6c012037a9170eb72c01718fe4d83f921d9e9
Quentin <q.chalopin@gmail.com>
2022-04-12 15:41:39 +0200

    test interface web

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+from sklearn import preprocessing
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('whine_model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    feature_list = request.form.to_dict()
+    final_features = list(feature_list.values())
+
+    prediction = model.predict(np.array([final_features]))
+
+    return render_template('index.html', prediction_text='Employee Income is {}'.format(prediction[0]))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
