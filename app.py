dcf571bf6d7b8fdb12383e1ab6e9c3d0dc6cdf14
Quentin <q.chalopin@gmail.com>
2022-04-15 00:01:55 +0200

    maj app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import numpy as np
 import lightgbm as lgb
 from create_df import acquisition, columns
 
@@ -16,16 +17,18 @@ def home():
 def predict():
     feature_list = request.form.to_dict()
 
+    pred_period = model.predict(acquisition(
+        columns,
+        1,
+        2254,
+        feature_list['airline'],
+        7,
+        feature_list['origin'],
+        feature_list['dest']
+    ))
+
     return render_template('prediction.html', prediction_text='Your flight have {}% chance to be delayed'.format(
-        model.predict(acquisition(
-            columns,
-            1,
-            2354,
-            feature_list['airline'],
-            7,
-            feature_list['origin'],
-            feature_list['dest']
-        ))
+        pred_period
     ))
 
 
